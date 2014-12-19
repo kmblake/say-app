0b29b5cc0874f2b828930b166b065c1c48184679
kmblake <kentblake@Kents-MacBook-Air.local>
2014-12-18 22:51:25 -0500

    Added CanCanCan gem for authorization

--- say/app/models/user.rb
+++ say/app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
 
   ROLES = %w[submitter editor admin]
 
+  def role?(base_role)
+    ROLES.index(base_role.to_s) <= ROLES.index(role)
+  end
+
   def name 
     first_name + " " + last_name
   end
