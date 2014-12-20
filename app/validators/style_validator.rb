f9356881fcc2f034344867aebcb7a674331bb960
kmblake <kentblake@Kents-MacBook-Air.local>
2014-12-20 17:36:01 -0500

    Added help page

--- app/validators/style_validator.rb
+++ app/validators/style_validator.rb
@@ -1,6 +1,6 @@
 class StyleValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    result = Document.select(:style).where(user_id: record.user_id).where(style: value)
+    result = Document.select(:style).where(user_id: record.user_id).where(style: value).where.not(id: record.id)
     unless result.empty?
       record.errors[attribute] << (options[:message] || "already exists.  You may only have one submission of each style")
     end
