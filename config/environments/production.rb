6b4c24fbd995e42b9bce1b92b9f00e98ff17d092
kmblake <kentblake@Kents-MacBook-Air.local>
2014-12-30 01:29:01 -0500

    Changed back SendGrid setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp 
 
   # Action mailer setup
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
