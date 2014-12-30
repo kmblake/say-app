24d4f3302fc43ba183e1bc52cef2b4410caff8de
kmblake <kentblake@Kents-MacBook-Air.local>
2014-12-29 23:40:28 -0500

    Added setting for SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,13 @@ Rails.application.configure do
 
   # Action mailer setup
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: ENV['gmail_username'],
-    password: ENV['gmail_pw']
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   config.paperclip_defaults = {
