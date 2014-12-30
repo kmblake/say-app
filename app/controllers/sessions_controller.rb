cbf63b7a3be79c9741233e8b40c0c539701343e7
kmblake <kentblake@Kents-MacBook-Air.local>
2014-12-30 01:12:42 -0500

    Deleted bad session controller

--- app/controllers/sessions_controller.rb
+++
@@ -1,5 +0,0 @@
-class SessionsController < Devise::SessionsController
-  rtn = super
-  sign_in(resource.role.underscore, resource.role.constantize.send(:find, resource.id)) unless resource.role.nil?
-  rtn
-end
