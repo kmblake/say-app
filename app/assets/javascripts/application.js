e68abbf9bfb555f7f0bca048c97aef562aed0971
kmblake <kentblake@Kents-MacBook-Air.local>
2015-03-20 10:47:54 +0000

    Switch appearance working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-switch
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require select2
+//= require bootstrap-switch
 //= require_tree .
