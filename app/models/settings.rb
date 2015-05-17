e5996c166ebb19d9d20c1ccae7478bee2317ad20
kmblake <kentblake@campus-066-039.ucdavis.edu>
2015-05-16 18:21:05 -0700

    Added artwork and emails to cvs

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,3 +1,15 @@
 class Settings < RailsSettings::CachedSettings
-  
+  def self.create_csv type  
+    puts "Hi"
+    require 'csv'     
+    @accepted_users = Submitter.joins(type).where("accepted = true").select("first_name", "last_name", "bio", "school", "teacher", "{type}.title")
+    puts @accepted_users
+    csv_string = CSV.generate do |csv|
+       csv << ["First Name", "Last Name", "Bio", "School", "Teacher", "Title"]
+       @accepted_users.each do |user|
+         csv << [user.first_name, user.last_name, user.bio, user.school, user.teacher, user.title]
+       end
+    end
+    puts csv_string
+  end      
 end
