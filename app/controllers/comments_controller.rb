77de56cf9099e972ab96fc81bca1956853474772
kmblake <kentblake@Kents-MacBook-Air.local>
2016-01-01 17:36:00 -0500

    Comments javascript for docs working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,29 +7,62 @@ class CommentsController < ApplicationController
 		if !params[:document_id].nil?
 	    	@comment.document_id = params[:document_id]
 	    	@document = Document.find(params[:document_id])
+        @submission = @document
 			if @comment.save 
-				redirect_to document_path(@document)
-			else
-				if Rating.exists?(:user_id => current_user.id, :document_id => @document.id)
-      				@rating = Rating.find_by_user_id_and_document_id(current_user.id, @document.id)
-    			else
-      				@rating = Rating.new
-    			end
-				render :template => 'documents/show'
-			end
+        respond_to do |format|
+          format.js {
+           render :template => "comments/update-comments.js.erb"
+          }
+        end
+      else
+        render json: { errors: @comment.errors.full_messages }, status: 422
+				# redirect_to document_path(@document)
+      end
 		elsif !params[:artwork_id].nil?
-			@comment.artwork_id = params[:artwork_id]
-			@artwork = Artwork.find(params[:artwork_id])
-			if @comment.save
-				redirect_to artwork_path(@artwork)
-			else
-				if Rating.exists?(:user_id => current_user.id, :artwork_id => @artwork.id)
-      				@rating = Rating.find_by_user_id_and_document_id(current_user.id, @artwork.id)
-    			else
-      				@rating = Rating.new
-    			end
-				render :template => 'artworks/show'
+      @comment.artwork_id = params[:artwork_id]
+      @artwork = artwork.find(params[:artwork_id])
+      @submission = @artwork
+      if @comment.save 
+        respond_to do |format|
+          format.js {
+           render :template => "comments/update-comments.js.erb"
+          }
+        end
+      else
+        render json: { errors: @comment.errors.full_messages }, status: 422
+        # redirect_to document_path(@document)
+      end
+
+			# @comment.artwork_id = params[:artwork_id]
+			# @artwork = Artwork.find(params[:artwork_id])
+			# if @comment.save
+			# 	redirect_to artwork_path(@artwork)
+			# else
+			# 	if Rating.exists?(:user_id => current_user.id, :artwork_id => @artwork.id)
+   #    				@rating = Rating.find_by_user_id_and_document_id(current_user.id, @artwork.id)
+   #  			else
+   #    				@rating = Rating.new
+   #  			end
+			# 	render :template => 'artworks/show'
 			end
 		end				
 	end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    @submission 
+    if @comment.document_id
+      @submission = Document.find(@comment.document_id)
+    else 
+      Artwork.find(@comment.artwork_id)
+    end
+    respond_to do |format|
+      format.js {
+       render :template => "comments/update-comments.js.erb"
+      }
+    end
+    # response here
+  end
+
 end
