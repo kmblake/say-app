ad234ba8b2fb4775bb900dcda6e89a91ffd6b070
kmblake <kentblake@Kents-MacBook-Air.local>
2016-01-01 20:09:16 -0500

    Fixed ratings not saving issue

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,13 +1,15 @@
 class RatingsController < ApplicationController
 	def create
 		@rating = Rating.new	
-    	@rating.user_id = current_user.id
+    @rating.user_id = current_user.id
 		@rating.rating_val = params[:rating][:rating_val]
 		if !params[:document_id].nil?
+			@rating.document_id = params[:document_id]
 			@rating.rating_val = params[:rating][:rating_val]
 			@rating.save 
 			render json: @rating
 		elsif !params[:artwork_id].nil?
+			@rating.artwork_id = params[:artwork_id]
 			@rating = Rating.find_by_user_id_and_artwork_id(current_user.id, params[:artwork_id])
 			@rating.rating_val = params[:rating][:rating_val]
 			@rating.save 
