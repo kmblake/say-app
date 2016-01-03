938824cd2a4574c22e6c9346c8109c1096ab9aeb
kmblake <kentblake@Kents-MacBook-Air.local>
2016-01-02 22:44:09 -0500

    Fixed new editor redirect loop

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -5,7 +5,7 @@ class Artwork < ActiveRecord::Base
   has_attached_file :image, :styles => {:medium => "800x800>", :thumb => "100x100>" }
 
   validates_attachment_presence :image
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   validates :title, :user_id, presence: true
 
   MAX_ARTWORKS = 3
