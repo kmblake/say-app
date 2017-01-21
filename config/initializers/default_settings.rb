0a0c76f5ff7c6406f3210d09ee14d73908bf801f
Kent Blake <kmblake@stanford.edu>
2017-01-21 00:19:14 -0800

    Close submissions

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -4,7 +4,7 @@ def populate_if_nil(setting, default)
   end
 end
 
-Settings.defaults[:accepting_submissions] = true
+Settings.defaults[:accepting_submissions] = false
 Settings.defaults[:finalized] = false
 Settings.defaults[:show_ratings] = false
 # Settings.defaults[:schools] = ["49ers Academy", "Blach Middle School", "Bullis Charter School", "California School for the Deaf", "Central Middle School (San Carlos)", "Chaboya Middle School", "Citizen Schools/Cesar Chavez", "Coliseum College Prep Academy", "Creekside Middle School", "East Palo Alto Charter School", "East Palo Alto Phoenix Academy", "El Portal Middle School", "Harvest Park Middle School", "Homeschooled", "Hopkins Junior High", "Horner Junior High", "JLS Middle School", "Joaquin Miller Middle School", "Joaquin Moraga Intermediate", "Jordan Middle School", "Kennedy Middle School", "Marin Teen Poets", "Martin Luther King Jr Middle School", "Mathson Middle School", "Montera Middle School", "Nea CLC", "North Star Academy", "Other", "Peterson Middle School", "Pine Valley Middle School", "Pleasanton Middle School", "Ralston Middle School", "Raymond J Fisher Middle School", "River Glen School", "Ronald McNair Middle School", "Saint Raymond School", "Seven Hills School", "Sierramont Middle School", "Sunol Glen School", "Synapse School ", "Thomas S Hart Middle School", "Tierra Linda Middle School", "Union Middle School", "Valley View Charter Prep", "Willow Oaks"]
